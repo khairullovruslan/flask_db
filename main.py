58de97befdede4dd99db49eb7ffaadda9d6f0e30
khairullovruslan <khairullov.ruslan@gmail.com>
2022-02-27 12:43:45 +0300

    2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from data import db_session
+from data.jobs import Jobs
 from data.users import User
 
 app = Flask(__name__)
@@ -9,31 +10,6 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def user_add():
-    user = User()
-    user.name = "Пользователь 1"
-    user.about = "биография пользователя 1"
-    user.email = "email@email.ru"
-    user.set_password("123456")
-
-    user2 = User(name="Пользователь 2",
-                 about="биография пользователя 2",
-                 email="email2@email.ru")
-    user2.set_password("1234")
-
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.add(user2)
-    db_sess.commit()
-
-
-def user_get():
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == 1).first()
-    for news in user.news:
-        print(news.created_date)
-
-
-def news_add():
     db_sess = db_session.create_session()
     user = User()
     user.surname = 'Scott'
@@ -45,12 +21,55 @@ def news_add():
     user.email = 'scott_chief@mars.org'
 
     db_sess.add(user)
+    user = User()
+    user.surname = 'Иван'
+    user.name = 'Петров'
+    user.age = 20
+    user.position = 'Рядовой'
+    user.speciality = 'engineer'
+    user.address = 'module_2'
+    user.email = 'ivan@mars.org'
+
+    db_sess.add(user)
+    user = User()
+    user.surname = 'Дима'
+    user.name = 'Петров'
+    user.age = 16
+    user.position = 'captain'
+    user.speciality = 'engineer'
+    user.address = 'module_1'
+    user.email = 'petrov@mars.org'
+
+    db_sess.add(user)
 
     db_sess.commit()
+
+def jobs_add():
+    db_sess = db_session.create_session()
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
+    db_sess.add(job)
+    db_sess.commit()
+
+
+def user_get():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == 1).first()
+    for news in user.news:
+        print(news.created_date)
+
+
+def news_add():
+    pass
 
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    # user_add()
+    user_add()
     news_add()
+    jobs_add()
 #  app.run(port=8080, host='127.0.0.1')
